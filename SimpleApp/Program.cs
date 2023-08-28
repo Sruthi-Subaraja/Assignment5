8c95aaaae25ea27bc1da0dcec7749556db5ae4ee
Sruthi-Subaraja <sruthi.subaraja@solitontech.com>
2023-08-28 17:20:14 +0530

    fix: greeting mars instead of world

--- SimpleApp/Program.cs
+++ SimpleApp/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Mars");
             Console.WriteLine("Hello, Saturn!");
             Console.WriteLine("Hello from the feat-branchOne");
         }
